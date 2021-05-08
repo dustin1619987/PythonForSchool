f0341563ede49aa3130fefad360afc3c97ec502c
Dustin101 <68936871+dustin1619987@users.noreply.github.com>
2021-05-08 02:10:07 -0400

    Create 1.9 LAB: Input: Mad Lib.py

---
+++ zyBooks/1.9 LAB: Input: Mad Lib.py
@@ -0,0 +1,8 @@
+#Input are here
+first_name = input()
+generic_location = input()
+whole_number = input()
+plural_noun = input()
+
+#Print Statement: _went to_to buy_ different type of_
+print(first_name, 'went to', generic_location, 'to buy', whole_number, 'different types of', plural_noun)
