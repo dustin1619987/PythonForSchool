a437bf5be77346a4956e81eca9380cd43c93aa09
Dustin101 <68936871+dustin1619987@users.noreply.github.com>
2021-05-08 19:40:40 -0400

    Create 1.21.1: LAB: Divide by x.py

---
+++ zyBooks/1.21.1: LAB: Divide by x.py
@@ -0,0 +1,11 @@
+# Start of my code
+import math
+
+user_num = int(input()) #interger user_num
+x = int(input()) #x as input
+#for i in range (3): <=ranges of the numer of 3 could use to make code shorter
+user_num1 = user_num // x  # // rounds down - if not then it would come back .number
+user_num2 = user_num1 // x 
+user_num3 = user_num2 // x
+print(user_num1, user_num2, user_num3) # output all the numbers 
+#End of my code
