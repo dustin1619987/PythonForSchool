9dc6dc711220f33ec19d46245c41616c5354dfe6
Dustin101 <68936871+dustin1619987@users.noreply.github.com>
2021-05-16 06:31:58 -0400

    Create NameAge.py

---
+++ zyBooks/NameAge.py
@@ -0,0 +1,6 @@
+# Start of Code: NameAge.py
+name = str(input("What is your name? ")) # Input name
+age = int(input("How old are you? ")) #Input Age
+date = 2020 - age # Math for the code
+print('Hello', name, '!', 'You were born in', date, '.') # Prints name and date
+# End of Code.
