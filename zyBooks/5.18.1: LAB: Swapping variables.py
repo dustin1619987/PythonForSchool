c35af36fc56c95e0189abffb1f70ed1a47086bfc
Dustin101 <68936871+dustin1619987@users.noreply.github.com>
2021-06-04 23:56:26 -0400

    Create 5.18.1: LAB: Swapping variables.py

---
+++ zyBooks/5.18.1: LAB: Swapping variables.py
@@ -0,0 +1,8 @@
+#Start of Code
+def swap_values(user_val1, user_val2): #Copied the statement from above
+    return user_val2, user_val1  #the statment above but adding a return which is opposite then the other
+if __name__ == '__main__':  #Variable being set for main program
+    user_val2 = input() #what helps with swapping
+    user_val1 = input() #Has to have 2 first then 1
+    print( user_val1, user_val2) #print out the variable which gets swapped
+#End of code
