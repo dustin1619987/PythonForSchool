9cbf57258f41127f07230819898cf27b19b11bc7
Dustin101 <68936871+dustin1619987@users.noreply.github.com>
2021-06-13 03:18:53 -0400

    Create 6.18.1: LAB: Word frequencies.py

---
+++ zyBooks/6.18.1: LAB: Word frequencies.py
@@ -0,0 +1,8 @@
+#Start of Code
+vocab = input() #input for vocabulary(vocab for short)
+index = vocab.split(" ") #defindes index. Don't forget the split-if so then it will do one letter per line and number
+for x in index:#for loop in index
+    print(x,index.count(x)) #cound and prints all the numbers out
+#End of Code
+
+#input hey hi Mark hi mark
